4a3aee3724a700ea341ccd5f9d61475f7ab3c9c2
Cecilia <ctong10@uw.edu>
2022-12-14 15:49:41 -0800

    completed repeat function exercise

--- higher-order.js
+++ higher-order.js
@@ -1,7 +1,18 @@
+/**
+ * Cecilia Tong
+ * Class 9 Exercise 
+ * Dec 13, 2022
+ * HIGHER ORDER FUNCTIONS
+ */
+
 // This function should execute the callback function the number of times specified.
 // When the function is being executed, the repetition number (i.e. 1 for the first call)
 // should be passed to the callback.
-const repeatFn = (times, callback) => {};
+const repeatFn = (times, callback) => {
+for (let i=0; i< times; i++){
+  callback();
+}  
+};
 
 // Test repeatFn
 const addButton = num => {
@@ -11,12 +22,12 @@ const addButton = num => {
 };
 repeatFn(6, addButton);
 
-const toThePower = (num, pow) => {
-  let product = 1;
-  repeatFn(pow, () => {
-    product += product * num;
-  });
-  return product;
-};
+// const toThePower = (num, pow) => {
+//   let product = 1;
+//   repeatFn(pow, () => {
+//     product += product * num;
+//   });
+//   return product;
+// };
 
-console.log(toThePower(3, 3));
+// console.log(toThePower(3, 3));
